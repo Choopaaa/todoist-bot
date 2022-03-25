86153845a42f3a30a356aab4337c519fad33f311
Ilya Chupin <havi_08@bk.ru>
2022-03-25 18:47:37 +0300

    Обработка ошибок, проверка статус кода

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+from logging import exception
 import requests
 
 
@@ -11,33 +12,44 @@ class BearerAuth(requests.auth.AuthBase):
 # Получение списка задач
 def get_tasks():
 
-    response = requests.get('https://api.todoist.com/rest/v1/tasks', auth=BearerAuth('839f660cc572bd387ef85163b740e334d1fa768a'))
-    response_json = response.json()
+    try:
+        response = requests.get('https://api.todoist.com/rest/v1/tasks', auth=BearerAuth('839f660cc572bd387ef85163b740e334d1fa768a'))
+        response_json = response.json()
 
-    return response_json
-
+        return response_json
+    except Exception as error:
+        print(error)
 # Добавление новой задачи
 def add_tasks(message):
     
-    response1 = requests.post(
-        'https://api.todoist.com/rest/v1/tasks', 
-        data = {'content': message}, 
-        auth=BearerAuth('839f660cc572bd387ef85163b740e334d1fa768a')
-    )
-    response1_json = response1.json()
-    
-    return response1_json
+    try:
+        response1 = requests.post(
+            'https://api.todoist.com/rest/v1/tasks', 
+            data = {'content': message}, 
+            auth=BearerAuth('839f660cc572bd387ef85163b740e334d1fa768a')
+        )
+        response1_json = response1.json()
+
+        if response1.status_code == 200:
+            print(True)
+
+        return response1_json
+    except Exception as error:
+        print(error)
 
 # Удаление задачи
 def delete_tasks(task_id):
 
-    response2 = requests.delete(f'https://api.todoist.com/rest/v1/tasks/{task_id}', auth=BearerAuth('839f660cc572bd387ef85163b740e334d1fa768a'))
-    response2_json = response2.json()
+    try:
 
-    return response2_json
+        response2 = requests.delete(f'https://api.todoist.com/rest/v1/tasks/{task_id}', auth=BearerAuth('839f660cc572bd387ef85163b740e334d1fa768a'))
+        response2_json = response2.json()
 
-print(get_tasks())
-print(add_tasks())
+        if response2.status_code == 204:
+            print(True)
 
-
+        return response2_json
+        
+    except Exception as error:
+        print(error)
 
